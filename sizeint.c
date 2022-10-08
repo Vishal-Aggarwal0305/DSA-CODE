282bd18d80d39b0bd742fab1e758bb691effcc6f
vishal03052001 <72963634+vishal03052001@users.noreply.github.com>
2022-10-08 13:09:18 +0530

    Create sizeint.c

---
+++ sizeint.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+    int a;
+    float f;
+    double d;
+    char c;
+    printf("Size of int: %d bytes\n", sizeof(a));
+    printf("Size of float: %d bytes\n", sizeof(f));
+    printf("Size of double: %d bytes\n", sizeof(d));
+    printf("Size of char: %d byte\n", sizeof(c));
+    return 0;
+}
