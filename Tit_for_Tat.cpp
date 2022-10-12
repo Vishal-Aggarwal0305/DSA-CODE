12b52d3faec85ab6bb360288fd44a3399f8077fa
kandhwayg <115629238+kandhwayg@users.noreply.github.com>
2022-10-12 19:57:38 +0530

    Added two codeforces solution to it.
    Please merge it and add hacktober accepted label to it.

---
+++ Tit_for_Tat.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        int ans=0;
+        for(int i=0;s[i];i++)
+        {
+            if(isupper(s[i]))
+            ans++;
+
+        }
+        cout<<ans<<endl;
+    }
+
+return 0;
+}
