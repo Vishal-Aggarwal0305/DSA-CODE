24962d2ef28d167d10011023f0ea4bc7adaeb606
kandhwayg <115629238+kandhwayg@users.noreply.github.com>
2022-10-12 19:59:11 +0530

    Added two codeforces solution to it.
    Please merge it and add hacktober accepted label to it.

---
+++ two_regular_polygons.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+int main(){
+int t;
+cin>>t;
+while(t--)
+{
+    int m,n;
+    cin>>n>>m;
+    if(n%m==0)
+    {
+        cout<<"YES"<<endl;
+    }
+    else
+    {
+        cout<<"NO"<<endl;
+    }
+}
+return 0;
+}
